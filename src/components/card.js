3ae574603c6e59cd1abb771edaf4891497e46ab5
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2020-10-20 04:47:43 -0300

    First commit

---
+++ src/components/card.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+class Card extends React.Component {
+ 
+    render() {
+        return(
+            <div class="card mb-3">
+               <h3 class="card-header">{this.props.title}</h3>
+
+               <div class="card-body">
+                  
+                  {this.props.children}
+
+
+               </div>
+            </div>
+
+        )
+    }
+
+}
+
+export default Card
